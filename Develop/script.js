f41b51e977399bc83f5c1729bf8cbc92355058db
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-01-26 19:22:00 -0700

    back up before adding js elements

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+// today displayed 
+
+//time blocks color coded to indicate past/present/future
+
+//save text in timeblock 
+
+//save to localStorage 
+
+//load localStorage to timeblock on refresh
